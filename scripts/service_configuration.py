ee2e61068ab476e000cb111bf25b13c7f862d4f2
ramses <slonik256@yandex.ru>
2020-10-21 16:30:14 +0300

    added conf

---
+++ scripts/service_configuration.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import time
+
+configuration_number = 1191.929291819
+
+print("service package 1: start configuration ")
+time.sleep(2)
+print("service package 1: successfully configured!")
+print("service package 1: configuration number : {}".format(configuration_number))
