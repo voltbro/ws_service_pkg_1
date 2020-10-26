7e756744089c6c0ee611153e86d21d8ed60e2059
ramses <slonik256@yandex.ru>
2020-10-26 16:40:47 +0300

    version 0.3.3

--- scripts/compile_test.py
+++ scripts/compile_test.py
@@ -1,6 +1,6 @@
 import compileall
 
-compileall.compile_file('service_configuration.py', force=True)
+compileall.compile_file('service_configuration.py', force=True, ddir='~/catkin_ws/src/ws_service_pkg_1/run/')
 
 # # Perform same compilation, excluding files in .svn directories.
 # import re
--- scripts/service_configuration.py
+++ scripts/service_configuration.py
@@ -3,13 +3,16 @@
 import time
 import tqdm
 
-configuration_number = 0.137508
-version = "0.3.2"
+configuration_number = -89.8
+version = "0.3.3"
 
 print("Service package 1: ver. {}".format(version))
+time.sleep(0.5)
 print("Service package 1: start configuration ")
+time.sleep(0.5)
 for i in tqdm.tqdm(range(10000), ascii=True, desc="System check"):
     time.sleep(0.001)
 
 print("Service package 1: successfully configured!")
+time.sleep(0.5)
 print("Service package 1: configuration checksum : {}".format(configuration_number))
